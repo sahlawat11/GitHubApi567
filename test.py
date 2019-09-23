70a3d7bbbd366c2bf5eddbc8edd56341460cf6ce
Saransh Ahlawat <Saransh.Ahlawat@viacomcontractor.com>
2019-09-22 22:36:00 -0400

    Fixed an error in travis yml file

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ import os
 
 if __name__ == '__main__':
     dir_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
-    print_pretty_table(f"************: {dir_path}")
+    print(f"************: {dir_path}")
