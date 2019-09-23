5d059b60d4580ee4bde84a1cf276646362c74f4b
Saransh Ahlawat <Saransh.Ahlawat@viacomcontractor.com>
2019-09-22 22:46:28 -0400

    .

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ import os
 
 if __name__ == '__main__':
     dir_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
-    print(f"************: {dir_path}")
+    print(f"************")
