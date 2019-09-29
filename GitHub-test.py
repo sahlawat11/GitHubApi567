f11c2358e90847996355becb780f144134330c98
Saransh Ahlawat <Saransh.Ahlawat@viacomcontractor.com>
2019-09-28 22:18:44 -0400

    .

--- GitHub-test.py
+++ GitHub-test.py
@@ -1,18 +1,38 @@
 import json
 import unittest
-
-from GitHub import get_github_repos_commits, print_user_repo_commits
+from unittest.mock import patch, Mock
+import os 
+from GitHub import get_git_repos, get_git_repo_commits
 
 class GitHubTests(unittest.TestCase):
 
     MY_REPO_ID = "richkempinski"
-    def test_get_github_repos_commits(self):
 
-        expected_repo_data = {'Mocks': 10, 'Project1': 2, 'hellogitworld': 30, 'helloworld': 6, 'threads-of-life': 1}
-        repos = get_github_repos_commits(self.MY_REPO_ID)
-        print_user_repo_commits(self.MY_REPO_ID)
-        self.assertEqual(repos, expected_repo_data)
-        
+
+    def test_request_response_with_patcher(self):
+        """Mocking using a patcher"""
+        mock_get_git_repos_patcher = patch('GitHub.get_git_repos')
+        mock_get_git_repos = mock_get_git_repos_patcher.start()
+
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(f"{dir_path}/repo-data.json") as file:
+            mock_get_git_repos.return_value = json.load(file)
+        mock_get_git_repos_patcher.stop()
+        repos = mock_get_git_repos()
+
+        self.assertEqual(repos[0].get("node_id"), "MDEwOlJlcG9zaXRvcnkyODc2NTc5MQ==")
+
+        mock_get_git_repos_commits_patcher = patch('GitHub.get_git_repo_commits')
+        mock_get_git_repos_commits = mock_get_git_repos_commits_patcher.start()
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(f"{dir_path}/repo-commits-data.json") as file:
+            mock_get_git_repos_commits.return_value = json.load(file)
+        mock_get_git_repos_commits_patcher.stop()
+        repo_commits = mock_get_git_repos_commits()
+
+        self.assertEqual(len(repo_commits), 30)
+        self.assertEqual(repo_commits[1].get("node_id"), "MDY6Q29tbWl0Mjg3NjU3OTE6M2E5MDc5MDFhZGQ5YTBkMzhlNmExOTkyNTFkZDNkZjBiMTczZWY2Zg==")
+
 
 if __name__ == "__main__":
     unittest.main()
--- GitHub.py
+++ GitHub.py
@@ -10,16 +10,16 @@ import json
 def get_github_repos_commits(github_user_id):
     repositories = dict()
     
-    user_repos = requests.get(f"https://api.github.com/users/{github_user_id}/repos")
+    user_repos = get_git_repos(github_user_id)
     parsed_user_repos = json.loads(user_repos.text)
 
     for repo in parsed_user_repos:
         repo_name = repo["name"]
-
-        repo_commits_raw = requests.get(f"https://api.github.com/repos/{github_user_id}/{repo_name}/commits")
+        repo_commits_raw = get_git_repo_commits(github_user_id, repo_name)
+        print(repo_commits_raw.text)
         repo_commits = json.loads(repo_commits_raw.text)
-
         repositories[repo_name] = len(repo_commits)
+
     return repositories
 
 
@@ -29,5 +29,13 @@ def print_user_repo_commits(github_user_id):
         print(f"Repo: {repo_name} Number of commits: {repo.get(repo_name)}")
 
 
+def get_git_repos(github_user_id):
+    user_repos = requests.get(f"https://api.github.com/users/{github_user_id}/repos")
+    return user_repos
+
+def get_git_repo_commits(github_user_id, repo_name):
+    repo_commits = requests.get(f"https://api.github.com/repos/{github_user_id}/{repo_name}/commits")
+    return repo_commits
+
 if __name__ == '__main__':
-    print_user_repo_commits("richkempinski")
+    get_github_repos_commits("richkempinski")
